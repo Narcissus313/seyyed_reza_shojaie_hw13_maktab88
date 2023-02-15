4ce8383df3115f14ce7471b178755cf4d2002625
narcissus313 <srz.shl@gmail.com>
2023-02-15 22:34:41 +0330

    1_5 completed

---
+++ 1_5/main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+let paths = [];
+
+for (let i = 1; i <= 5; i++) {
+	const filePath = path.join(__dirname, "/pages/page-" + i + ".html");
+	paths.push(filePath);
+	app.get("/page-" + i, (_req, res) => {
+		res.sendFile(paths[i - 1]);
+	});
+}
+
+app.listen(80, (err) => {
+	if (err) console.log("err=> ", err);
+});
