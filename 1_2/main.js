01d8e993e9d566892bd8b4da681fbe0784ffdcc5
narcissus313 <srz.shl@gmail.com>
2023-02-15 16:51:10 +0330

    2_2 ended using app.use

--- 1_2/main.js
+++ 1_2/main.js
@@ -9,24 +9,18 @@ const websiteParts = [
 	{ url: "contact", message: "Contact Route" },
 ];
 
-// let requestedPart;
-// app.use((req, res, next) => {
-// 	requestedPart = req.url.trim().split("/")[1];
-// 	// console.log("requestedPart: ", requestedPart);
-// 	const targetPart = websiteParts.find((part) => part.url === requestedPart);
-// 	if (targetPart) {
-// 		res.send(targetPart.message);
-// 		return;
-// 	}
-// 	res.send("not found");
-// 	next();
-// });
-
-for (const section of websiteParts) {
-	app.get("./" + section.url, (_req, res) => {
-		res.send(section.message);
-	});
-}
+let requestedPart;
+app.use((req, res, next) => {
+	requestedPart = req.url.trim().split("/")[1];
+	// console.log("requestedPart: ", requestedPart);
+	const targetPart = websiteParts.find((part) => part.url === requestedPart);
+	if (targetPart) {
+		res.send(targetPart.message);
+		return;
+	}
+	res.send("not found");
+	next();
+});
 
 app.listen(80, (err) => {
 	if (err) console.log("err: ", err);
